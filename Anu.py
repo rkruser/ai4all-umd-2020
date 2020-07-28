84cf67d9dca69915168ffd4d7c865e41bd5ff203
anunair123 <anunair1933@gmail.com>
2020-07-28 19:47:30 +0000

    Added_Anu.py

--- Anu.py
+++ Anu.py
@@ -1,1 +1,46 @@
-#hello
+
+
+class YourNetwork(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, 64)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(2, 32, 32)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+
+    def forward(self, x):
+        pass
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+        
+epochs = range(0,2)
+plt.plot(epochs, loss_train, 'g', label='Training loss')
+plt.title('Training loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+print('Finished Training')
+#save the trained model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH
