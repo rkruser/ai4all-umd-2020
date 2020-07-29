6efa9d3d0ef654970a695f33892c23adff388f24
pcooper-coder <55923910+pcooper-coder@users.noreply.github.com>
2020-07-29 09:01:03 -0700

    Update project_network.py

--- project_network.py
+++ project_network.py
@@ -1,3 +1,4 @@
+# Importing libraries
 import torch
 from torchvision import models
 import torch.nn as nn
@@ -12,31 +13,40 @@ resnet18 = models.resnet18(pretrained=False, num_classes=185) # A pre-defined ne
 # Use the modules nn.Conv2d, nn.Linear, nn.ReLU, and other pytorch functions you deem necessary
 class YourNetwork(nn.Module):
    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5, 2)
-        self.pool = nn.MaxPool2d(2, 1)
+        super(Net, self).__init__()# Ask about
+         
+        # Apply 4 convultions and 3 max pools
+        self.conv1 = nn.Conv2d(3, 64, 5, 2) # Input channel, output channel, kernel size, stride
+        # First max pool
+        self.pool = nn.MaxPool2d(2, 1) # kernel size, stride
 
-        self.conv2 = nn.Conv2d(64, 128, 5, 2)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(64, 128, 5, 2)# Input channel, output channel, kernel size, stride
+        # Second max pool
+        self.pool2 = nn.MaxPool2d(2, 2) # kernel size, stride
+        self.conv3 = nn.Conv2d(128, 256, 5, 2)# Input channel, output channel, kernel size, stride  
+        self.conv4 = nn.Conv2d(256, 512, 5, 2)# Input channel, output channel, kernel size, stride
 
-        self.conv3 = nn.Conv2d(128, 256, 5, 2)
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.conv4 = nn.Conv2d(256, 512, 5, 2)
-
-        self.linLay1 = nn.Linear(512*224*224, 1024)
-        self.linLay2 = nn.Linear(1024, 512)
-        self.linLay3 = nn.Linear(512, 256)
-        self.linLay4 = nn.Linear(256, 185)
+        # Apply 4 linear layers
+        self.linLay1 = nn.Linear(512*224*224, 1024)# Input size(channels*pixels), output size
+        self.linLay2 = nn.Linear(1024, 512)# Input size, output size
+        self.linLay3 = nn.Linear(512, 256)# Input size, output size
+        self.linLay4 = nn.Linear(256, 185)# Input size, output size
 
     def forward(self, x):
+        # Call the max pools in terms of the applied convolutions
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool2(F.relu(self.conv3(x)))
+        x = self.pool2(F.relu(self.conv4(x)))
+         
+        # Resize the tensor so that the linear layers can use it
         x = x.view(-1, 512*224*224)
+         
+        # Apply the reLus to the linLays
         x = F.relu(self.linLay1(x))
         x = F.relu(self.linLay2(x))
         x = F.relu(self.linLay3(x))
         x = self.linLay4(x)
+      
+        # Return the final tensor
         return x
