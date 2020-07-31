7f32509f1d3956e701a3d44020bc570c52b4c65b
unitydill <68913983+unitydill@users.noreply.github.com>
2020-07-31 11:45:51 -0400

    Update Unity.py

--- Unity.py
+++ Unity.py
@@ -4,17 +4,7 @@ import torch.nn as nn # Neural network library
 from PIL import Image # Image class from python imaging library (PIL)
 import numpy as np
 
-
-def print_tensors(tensor_list):
-  for t in tensor_list:
-    print(t.size(), '\n', t)
-
-to_tensor = transforms.ToTensor()
-to_pil = transforms.ToPILImage()
-resize600 = transforms.Resize(500)
-grayscale = transforms.Grayscale()
-
-def apply_convolution(im_tensor, kernel, bias=None, stride=1, padding=0):
+def apply_convolution(im_tensor, kernel, bias=None, stride=1, padding=0, color=False):
   if len(kernel.size()) == 2:
     kernel = kernel.unsqueeze(0).unsqueeze(0)
   elif len(kernel.size()) == 3:
@@ -26,8 +16,7 @@ def rescale(im_tensor, perc1 = 20, perc2 = 98):
   pc1 = np.percentile(numpy_tensor, perc1)
   pc2 = np.percentile(numpy_tensor, perc2)
   return ((im_tensor-pc1)/(pc2-pc1)).clamp(0,1)
-     
-    
+
 def Unity_function(im):
   transform = transforms.Compose([
       transforms.Resize(600),
@@ -35,48 +24,30 @@ def Unity_function(im):
   ])
   to_pil = transforms.ToPILImage()
   im_tensor = transform(im)
-  
-!mkdir webims
-!curl -o ./webims/flower.jpg https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
 
-im_flower = Image.open('./webims/flower.jpg')
-resize600= transforms.Resize(500)
-im_flower = resize600(im_flower) #Scale longest dimension down to 500 pixels
-to_tensor= transforms.ToTensor()
-im_flower_tensor = to_tensor(grayscale(im_flower))
+  vertical_kernel = torch.Tensor([[[-1, 0, 1],
+                                  [-2, 0, 2],
+                                  [-1, 0, 1]],[[-1, 0, 1],
+                                  [-2, 0, 2],
+                                  [-1, 0, 1]],[[-1, 0, 1],
+                                  [-2, 0, 2],
+                                  [-1, 0, 1]]])
 
+  im_vertical_tensor = apply_convolution(im_tensor, vertical_kernel,color=True)
+  im_vertical_tensor = im_vertical_tensor.clamp(0,1)
 
-display(to_pil(im_flower_tensor))
-display(to_pil(rescale(im_flower_tensor,perc1=40,perc2=60)))
+  blur_kernel = torch.ones(10,10)
+  im_blur = apply_convolution(im_vertical_tensor, blur_kernel)
+  im_blur = rescale(im_blur, perc1=0, perc2=100)
 
-#to_pil = transforms.ToPILImage()
+  sliced_image=im_blur[:,:,:]
+  zeros = torch.zeros(sliced_image.size()[1:])
+  blue_slice = sliced_image[0,:,:]
+  blue_slice = torch.stack([blue_slice, zeros, zeros])
+  #display(to_pil(blue_slice))
+  return to_pil(blue_slice)
 
-#vertical_kernel = torch.Tensor([[-1, 0, 1],
-                               # [-2, 0, 2],
-                         #       [-1, 0, 1]])
-#im_flower_vertical_tensor = apply_convolution(im_flower_tensor, vertical_kernel)
-#im_flower_vertical_tensor = rescale(im_flower_vertical_tensor, perc1=20, perc2=98)
-#display(to_pil(im_flower_vertical_tensor))
-
-#return to_pil(im_tensor)
-
-#horizontal_kernel = torch.Tensor([[1, 2, 1],
-                                #  [0, 0, 0],
-                                #  [-1, -2, -1]])
-#im_flower_horizontal_tensor = apply_convolution(im_flower_tensor, horizontal_kernel)
-#im_flower_horizontal_tensor = rescale(im_flower_horizontal_tensor, perc1=20, perc2=98)
-#display(to_pil(im_flower_horizontal_tensor))
-
-#blur_kernel = torch.ones(10,10)
-#im_flower_blur = apply_convolution(im_flower_tensor, blur_kernel)
-#im_flower_blur = rescale(im_flower_blur, perc1=0, perc2=100)
-#display(to_pil(im_flower_blur))
-
-def test():
-    img = Image.open('./webims/flower.jpg')
-
-    transformed = Unity_transform(img)
-
-    display(img)
-
-#test()
+#!mkdir webims
+#!curl -o ./webims/image.jpg https://static01.nyt.com/images/2020/04/22/arts/22kimmelman-skyscrapers1/22kimmelman-skyscrapers1-videoSixteenByNineJumbo1600.jpg 
+#im = Image.open('./webims/image.jpg')
+#Unity_function(im)
