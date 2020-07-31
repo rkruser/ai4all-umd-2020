7337f62bf80c99b9b976f134584aa8b84d79b0ed
unitydill <68913983+unitydill@users.noreply.github.com>
2020-07-31 10:54:11 -0400

    Update Unity.py

--- Unity.py
+++ Unity.py
@@ -26,9 +26,9 @@ def rescale(im_tensor, perc1 = 20, perc2 = 98):
   pc1 = np.percentile(numpy_tensor, perc1)
   pc2 = np.percentile(numpy_tensor, perc2)
   return ((im_tensor-pc1)/(pc2-pc1)).clamp(0,1)
- ])    
+     
     
-    def Unity_function(im):
+def Unity_function(im):
   transform = transforms.Compose([
       transforms.Resize(600),
       transforms.ToTensor()                            
@@ -43,7 +43,7 @@ im_flower = Image.open('./webims/flower.jpg')
 resize600= transforms.Resize(500)
 im_flower = resize600(im_flower) #Scale longest dimension down to 500 pixels
 to_tensor= transforms.ToTensor()
-im_puppy_tensor = to_tensor(grayscale(im_flower))
+im_flower_tensor = to_tensor(grayscale(im_flower))
 
 
 display(to_pil(im_flower_tensor))
@@ -54,8 +54,8 @@ display(to_pil(rescale(im_flower_tensor,perc1=40,perc2=60)))
 #vertical_kernel = torch.Tensor([[-1, 0, 1],
                                # [-2, 0, 2],
                          #       [-1, 0, 1]])
-#im_flower_vertical_tensor = apply_convolution(im_puppy_tensor, vertical_kernel)
-#im_flower_vertical_tensor = rescale(im_puppy_vertical_tensor, perc1=20, perc2=98)
+#im_flower_vertical_tensor = apply_convolution(im_flower_tensor, vertical_kernel)
+#im_flower_vertical_tensor = rescale(im_flower_vertical_tensor, perc1=20, perc2=98)
 #display(to_pil(im_flower_vertical_tensor))
 
 #return to_pil(im_tensor)
@@ -63,14 +63,14 @@ display(to_pil(rescale(im_flower_tensor,perc1=40,perc2=60)))
 #horizontal_kernel = torch.Tensor([[1, 2, 1],
                                 #  [0, 0, 0],
                                 #  [-1, -2, -1]])
-#im_puppy_horizontal_tensor = apply_convolution(im_puppy_tensor, horizontal_kernel)
-#im_puppy_horizontal_tensor = rescale(im_puppy_horizontal_tensor, perc1=20, perc2=98)
-#display(to_pil(im_puppy_horizontal_tensor))
+#im_flower_horizontal_tensor = apply_convolution(im_flower_tensor, horizontal_kernel)
+#im_flower_horizontal_tensor = rescale(im_flower_horizontal_tensor, perc1=20, perc2=98)
+#display(to_pil(im_flower_horizontal_tensor))
 
 #blur_kernel = torch.ones(10,10)
-#im_puppy_blur = apply_convolution(im_puppy_tensor, blur_kernel)
-#im_puppy_blur = rescale(im_puppy_blur, perc1=0, perc2=100)
-#display(to_pil(im_puppy_blur))
+#im_flower_blur = apply_convolution(im_flower_tensor, blur_kernel)
+#im_flower_blur = rescale(im_flower_blur, perc1=0, perc2=100)
+#display(to_pil(im_flower_blur))
 
 def test():
     img = Image.open('./webims/flower.jpg')
