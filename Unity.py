e84e96a48a3a7ae7d0a4e0f9acc4674c56b58ec4
rkruser <rkrusinga@gmail.com>
2020-07-31 11:59:12 -0400

    Update Unity.py

--- Unity.py
+++ Unity.py
@@ -25,30 +25,30 @@ def Unity_function(im):
   to_pil = transforms.ToPILImage()
   im_tensor = transform(im)
 
-  vertical_kernel = torch.Tensor([[[-1, 0, 1],
-                                  [-2, 0, 2],
-                                  [-1, 0, 1]],[[-1, 0, 1],
-                                  [-2, 0, 2],
-                                  [-1, 0, 1]],[[-1, 0, 1],
-                                  [-2, 0, 2],
-                                  [-1, 0, 1]]])
+  #vertical_kernel = torch.Tensor([[[-1, 0, 1],
+  #                                [-2, 0, 2],
+  #                                [-1, 0, 1]],[[-1, 0, 1],
+  #                                [-2, 0, 2],
+  #                                [-1, 0, 1]],[[-1, 0, 1],
+  #                                [-2, 0, 2],
+  #                                [-1, 0, 1]]])
 
-  im_vertical_tensor = apply_convolution(im_tensor, vertical_kernel,color=True)
-  im_vertical_tensor = im_vertical_tensor.clamp(0,1)
+  #im_vertical_tensor = apply_convolution(im_tensor, vertical_kernel,color=True)
+  #im_vertical_tensor = im_vertical_tensor.clamp(0,1)
 
-  blur_kernel = torch.ones(10,10)
-  im_blur = apply_convolution(im_vertical_tensor, blur_kernel)
-  im_blur = rescale(im_blur, perc1=0, perc2=100)
+  #blur_kernel = torch.ones(10,10)
+  #im_blur = apply_convolution(im_vertical_tensor, blur_kernel)
+  #im_blur = rescale(im_blur, perc1=0, perc2=100)
 
-  sliced_image=im_blur[:,:,:]
+  sliced_image=im_tensor #im_blur[:,:,:]
   zeros = torch.zeros(sliced_image.size()[1:])
-  blue_slice = sliced_image[0,:,:]
-  blue_slice = torch.stack([blue_slice, zeros, zeros])
+  blue_slice = sliced_image[2,:,:]
+  blue_slice = torch.stack([zeros, zeros, blue_slice])
   #display(to_pil(blue_slice))
   return to_pil(blue_slice)
 
-!mkdir webims
-!curl -o ./webims/image.jpg https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+#!mkdir webims
+#!curl -o ./webims/image.jpg https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
 
-im = Image.open('./webims/image.jpg')
-Unity_function(im)
+#im = Image.open('./webims/image.jpg')
+#Unity_function(im)
